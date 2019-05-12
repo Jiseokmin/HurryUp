2602a01e50af8a0bc21d170898f0cdf244fa73d3
Jiseokmin <37jr@naver.com>
2019-05-13 01:55:37 +0900

    -자세 교정 운동 방향으로 정리 -앱 런쳐 아이콘 변경

--- app/src/main/java/kr/study/hurryup/SelectPoseActivity.java
+++ app/src/main/java/kr/study/hurryup/SelectPoseActivity.java
@@ -10,38 +10,21 @@ import android.widget.ImageButton;
 
 public class SelectPoseActivity extends AppCompatActivity {
 
-    private String[] pose_basic_list = {"공부 자세", "컴퓨터 자세", "..."};
-    private String[] pose_exercise_list = {"거북목 방지 운동", "운동 자세2", "운동 자세3"};
+    private String[] pose_exercise_list = {"자세 교정 운동1", "자세 교정 운동2", "자세 교정 운동3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_pose);
 
-        final ImageButton imagebtn_basic_pose = (ImageButton) findViewById(R.id.pose_basic);
-        final ImageButton imagebtn_exercise_pose = (ImageButton) findViewById(R.id.pose_exercise);
 
-        imagebtn_basic_pose.setOnClickListener(new ImageButton.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
-                dialog.setTitle("인식할 기본자세를 선택하세요.")
-                        .setItems(pose_basic_list, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int i) {
-                                // working with rasp camera
-                                // i 번 선택시 내용
-                            }
-                        });
-                AlertDialog alert = dialog.create();
-                alert.show();
-            }
-        });
+        final ImageButton imagebtn_exercise_pose = (ImageButton) findViewById(R.id.pose_exercise);
 
         imagebtn_exercise_pose.setOnClickListener(new ImageButton.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
-                dialog.setTitle("인식할 운동 자세를 선택하세요.")
+                dialog.setTitle("운동 자세를 선택하세요.")
                         .setItems(pose_exercise_list, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int i) {
 
